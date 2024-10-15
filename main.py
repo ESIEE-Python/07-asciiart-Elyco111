b2780cc7817b4c5a4503c8ec1c97124dc496841a
Baptiste Serrurier <93823010+Elyco111@users.noreply.github.com>
2024-10-15 07:35:50 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-#### Imports et définition des variables globales
-
-
-#### Fonctions secondaires
-
+'''
+coucou
+'''
+import sys
+sys.setrecursionlimit(2000)
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne 
+    de caractères passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,14 +14,30 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
 
-    return [ ]
+    if not s:
+        return []
+
+    result = []
+    current_char = s[0]
+    count = 1
+
+    for i in range(1, len(s)):
+        if s[i] == current_char:
+            count += 1
+        else:
+            result.append((current_char, count))
+            current_char = s[i]
+            count = 1
+
+    result.append((current_char, count))
+    return result
+
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne
+     de caractères passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -28,20 +45,22 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
 
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
+    if not s:
+        return []
 
-    return []
-    
+    count = 1
+    for i in range(1, len(s)):
+        if s[i] != s[0]:
+            break
+        count += 1
 
-#### Fonction principale
-
+    return [(s[0], count)] + artcode_r(s[count:])
 
 def main():
+    '''
+    coucou
+    '''
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
